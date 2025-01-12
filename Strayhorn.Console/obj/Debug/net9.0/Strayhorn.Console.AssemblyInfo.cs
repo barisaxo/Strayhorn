41b3a540c2871cc10edc71d7c002add2c0c74ab7
Pino <59021624+barisaxo@users.noreply.github.com>
2025-01-11 20:33:00 -0600

    Added 7th chords

--- Strayhorn.Console/obj/Debug/net9.0/Strayhorn.Console.AssemblyInfo.cs
+++ Strayhorn.Console/obj/Debug/net9.0/Strayhorn.Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Strayhorn.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b63cd45a11da24871535714663d3ec11780acc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d41150de552d67c08b3404bcf5401f4d15657b45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Strayhorn.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Strayhorn.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Strayhorn.Console/scripts/Main/MainMenu.cs
+++ Strayhorn.Console/scripts/Main/MainMenu.cs
@@ -3,11 +3,12 @@ namespace Strayhorn.Menus;
 public class MainMenu : IMenu
 {
     public IMenuItem Selection { get; set; } = Item1;
-    public IMenuItem[] MenuItems { get; } = [Item1, Item2, Item3, Item4, Item5, ItemX];
+    public IMenuItem[] MenuItems { get; } = [Item1, Item2, Item3, Item4, Item5, Item6, ItemX];
     readonly static MenuItem Item1 = new("Notes", () => new MenuState(new NotesMenu()));
     readonly static MenuItem Item2 = new("Steps", () => new MenuState(new StepsMenu()));
     readonly static MenuItem Item3 = new("Scales", () => new MenuState(new ScalesMenu()));
     readonly static MenuItem Item4 = new("Intervals", () => new MenuState(new IntervalsMenu()));
     readonly static MenuItem Item5 = new("Triads", () => new MenuState(new TriadsMenu()));
+    readonly static MenuItem Item6 = new("Triads", () => new MenuState(new SeventhChordsMenu()));
     readonly static MenuItem ItemX = new("Quit", () => new QuitState());
 }
