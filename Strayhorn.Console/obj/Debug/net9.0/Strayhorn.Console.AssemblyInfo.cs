25e93ca6d4c3ac92cf369736dd5e752b456f3b3b
Pino <59021624+barisaxo@users.noreply.github.com>
2025-01-13 12:37:12 -0600

    Removed Unused package
    NAudio doesn't work on mac

--- Strayhorn.Console/obj/Debug/net9.0/Strayhorn.Console.AssemblyInfo.cs
+++ Strayhorn.Console/obj/Debug/net9.0/Strayhorn.Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Strayhorn.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1ac0e9cfbca0990179589a06860a0788a8cabe0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1e95f52b01dc0e14a546a0e1c557befbe724a65")]
 [assembly: System.Reflection.AssemblyProductAttribute("Strayhorn.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Strayhorn.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
